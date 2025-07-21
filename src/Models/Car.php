437b4692ae04ef1b4d6da38875060b428e553a24
Mika-lab-coder <wursthorn.mika@gmail.com>
2025-07-21 14:24:43 +0200

    feat Car.php

--- src/Models/Car.php
+++ src/Models/Car.php
@@ -2,15 +2,28 @@
 namespace App\Models;
 use PDO;
 
-// model Cars, represente une voiture en bdd
+// model Car, represente une voiture en bdd
 
-class Cars extends BaseModel{
+class Car extends BaseModel{
     protected string $table = "cars";
 
+    /**
+     * recupere toutes les voitures
+     * @ return array tableau de voitures
+     */
     public function all():array{
         $stmt = $this->db->query("SELECT * FROM {$this->table} ORDER BY created_at DESC");
 
         //FETCH_ASSOC est deja defini dans notre class Database
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function find(int $car_id):array{
+        $stmt = $this->db->prepare("SELECT * FROM {$this->table} WHERE car_id = :id");
+        $stmt->execute([":id" => $car_id]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data ?: [];
+    }
+
+
 }
